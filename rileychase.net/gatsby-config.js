cdf65dd94e629f3573adac72ce83aaddc1230d16
Riley Chase <riley.chase@bulletproof.net>
2019-01-10 22:08:16 +1100

    add default gatsby starter site as base

---
+++ rileychase.net/gatsby-config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  siteMetadata: {
+    title: `RileyChase.net`,
+    description: `A place for all my encounters with technological insanity`,
+    author: `Riley Chase`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-offline',
+  ],
+}
