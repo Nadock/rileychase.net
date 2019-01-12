897d59d7832e2cfe64de162ecb8e319d68537fce
Riley Chase <riley.chase@bulletproof.net>
2019-01-13 01:54:16 +1100

    add better `set` options, add better doc comments

--- .actions/test_build_deploy/entrypoint.sh
+++ .actions/test_build_deploy/entrypoint.sh
@@ -1,6 +1,18 @@
-#!/bin/sh
+#!/usr/bin/env bash
 #
-# GitHub Action entry point file - runs defined tests in :/infra/Makefile
+# GitHub Action entry point
+#
+# On `*` branches, run tests.
+# On `dev` branch, run development environment deploy
+# On `master` branch, run production environment deploy
+
+
+# e: Exit immediately on fail
+# E: Inherit ERR trap so it works correctly if something fails and we exit because of -e
+# u: Treat unset variables as errors
+# x: Print each instruction to stderr before executing
+# o pipefail: Exit status of pipe is non-zero if any step in pipe fails
+set -Eeuxo pipefail
 
 # ${GITHUB_WORKSPACE} contains checked out a copy of the git commit that triggered this action
 cd ${GITHUB_WORKSPACE}/infra
