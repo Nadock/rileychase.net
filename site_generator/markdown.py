b024b30bfff3b7bd9b47ab1dec54454aca7fb40e
Riley Chase <riley.chase@rileychase.net>
2022-09-01 22:48:35 +0930

    Implement markdown loading and rendering logic

--- site_generator/markdown.py
+++ site_generator/markdown.py
@@ -3,6 +3,7 @@ import os
 import pathlib
 
 import yaml
+from pymdownx import emoji  # type: ignore
 
 import markdown
 
@@ -23,16 +24,69 @@ def find_markdown(path: pathlib.Path) -> list[pathlib.Path]:
     return markdown_paths
 
 
-def load_markdown(path: pathlib.Path):
-    md = markdown.Markdown(
-        extensions=["full_yaml_metadata"],
-        extension_configs={
-            "full_yaml_metadata": {"yaml_loader": yaml.SafeLoader},
-        },
+@dataclasses.dataclass
+class MarkdownFile:
+    """
+    A `MarkdownFile` is a collection of the raw Markdown formatted contents of a file
+    and any YAML frontmatter that is present.
+    """
+
+    content: str
+    frontmatter: dict
+
+    def render(self) -> str:
+        """Render the content of this `MarkdownFile` as HTML."""
+        return markdown.Markdown(
+            extensions=[
+                "markdown.extensions.tables",
+                "markdown.extensions.fenced_code",
+                "markdown.extensions.codehilite",
+                "pymdownx.betterem",
+                "pymdownx.emoji",
+                "pymdownx.highlight",
+                "pymdownx.magiclink",
+                "pymdownx.saneheaders",
+                "pymdownx.tasklist",
+                "pymdownx.tilde",
+                "nl2br",
+            ],
+            output_format="html",
+            extension_configs={
+                "pymdownx.emoji": {
+                    "emoji_index": emoji.gemoji,
+                    "emoji_generator": emoji.to_svg,
+                },
+                "pymdownx.tasklist": {
+                    "custom_checkbox": True,
+                },
+            },
+        ).convert(self.content)
+
+
+def load_markdown(path: pathlib.Path) -> MarkdownFile:
+    """
+    Read file at path and extract markdown content and any YAML frontmatter separately.
+
+    This does not parse the markdown content in any way, but does parse the YAML
+    frontmatter into a `dict` using `yaml.safe_load`.
+    """
+    with path.open("r", encoding="utf-8") as file:
+        lines = file.readlines()
+
+    if len(lines) < 2:
+        # Cannot have frontmatter with less than 2 lines
+        return MarkdownFile(content="".join(lines), frontmatter={})
+
+    # Detect end of frontmatter — idx points to frontmatter end delimiter line
+    idx = 0
+    if lines[0] == "---\n":
+        while idx < len(lines):
+            idx += 1
+            if idx + 1 < len(lines) and lines[idx] in ["---\n", "...\n"]:
+                idx += 1
+                break
+
+    return MarkdownFile(
+        content="".join(lines[idx:]),
+        frontmatter=yaml.safe_load("".join(lines[1 : idx - 1])),
     )
-
-    html = md.convert(path.read_text("utf-8"))
-    meta = md.Meta
-
-    print(f"load_markdown({path=}) => {html=}")
-    print(f"load_markdown({path=}) => {meta=}")
