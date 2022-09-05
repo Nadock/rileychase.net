dadf3b2a8488716302811a9b59de832de15fcac7
Riley Chase <riley.chase@rileychase.net>
2022-09-05 21:48:32 +0930

    Implement markdown page pipeline logic

--- site_generator/markdown.py
+++ site_generator/markdown.py
@@ -1,69 +1,55 @@
-import dataclasses
 import os
 import pathlib
+from typing import AsyncIterator, Tuple
 
 import yaml
 from pymdownx import emoji  # type: ignore
 
 import markdown
+from site_generator import config, template
 
 
-def find_markdown(path: pathlib.Path) -> list[pathlib.Path]:
+async def markdown_pipeline(
+    cfg: config.SiteGeneratorConfig, page: pathlib.Path
+) -> pathlib.Path:
+    """Process a markdown page into an HTML page."""
+    content, frontmatter = await load_markdown(page)
+
+    template_name = frontmatter.get("template", cfg.default_template)
+
+    output_path = frontmatter.get("path")
+    if not output_path:
+        base = page.parent.relative_to(cfg.pages)
+        name = page.parts[-1].replace(".md", ".html")
+        output_path = base / name
+    output_path = (cfg.output / output_path).absolute()
+    output_path.parent.mkdir(parents=True, exist_ok=True)
+
+    html = await template.render_template(
+        cfg.templates,
+        template_name,
+        content=content,
+        meta=frontmatter,
+    )
+
+    output_path.write_text(html)
+
+    return output_path
+
+
+async def find_markdown(path: pathlib.Path) -> AsyncIterator[pathlib.Path]:
     """
     Find any files Markdown files under a root `path`.
 
     Current implementation just checks for file names that end in `.md`.
     """
-    markdown_paths: list[pathlib.Path] = []
-
     for dirpath, _, filenames in os.walk(path):
         for filename in filenames:
             if filename.endswith(".md"):
-                markdown_paths.append(pathlib.Path(dirpath) / filename)
-
-    return markdown_paths
+                yield pathlib.Path(dirpath) / filename
 
 
-@dataclasses.dataclass
-class MarkdownFile:
-    """
-    A `MarkdownFile` is a collection of the raw Markdown formatted contents of a file
-    and any YAML frontmatter that is present.
-    """
-
-    content: str
-    frontmatter: dict
-
-    def render(self) -> str:
-        """Render the content of this `MarkdownFile` as HTML."""
-        return markdown.Markdown(
-            extensions=[
-                "markdown.extensions.tables",
-                "markdown.extensions.fenced_code",
-                "markdown.extensions.codehilite",
-                "pymdownx.betterem",
-                "pymdownx.emoji",
-                "pymdownx.highlight",
-                "pymdownx.magiclink",
-                "pymdownx.saneheaders",
-                "pymdownx.tasklist",
-                "pymdownx.tilde",
-                "nl2br",
-            ],
-            output_format="html",
-            extension_configs={
-                "pymdownx.emoji": {
-                    "emoji_index": emoji.gemoji,
-                    "emoji_generator": emoji.to_svg,
-                },
-                "pymdownx.tasklist": {
-                    "custom_checkbox": True,
-                },
-            },
-        ).convert(self.content)
-
-
-def load_markdown(path: pathlib.Path) -> MarkdownFile:
+async def load_markdown(path: pathlib.Path) -> Tuple[str, dict]:
     """
     Read file at path and extract markdown content and any YAML frontmatter separately.
 
@@ -75,7 +61,7 @@ def load_markdown(path: pathlib.Path) -> MarkdownFile:
 
     if len(lines) < 2:
         # Cannot have frontmatter with less than 2 lines
-        return MarkdownFile(content="".join(lines), frontmatter={})
+        return "".join(lines), {}
 
     # Detect end of frontmatter — idx points to frontmatter end delimiter line
     idx = 0
@@ -86,7 +72,37 @@ def load_markdown(path: pathlib.Path) -> MarkdownFile:
                 idx += 1
                 break
 
-    return MarkdownFile(
-        content="".join(lines[idx:]),
-        frontmatter=yaml.safe_load("".join(lines[1 : idx - 1])),
+    content = "".join(lines[idx:])
+    frontmatter = yaml.safe_load("".join(lines[1 : idx - 1]))
+
+    return content, frontmatter
+
+
+async def render(content: str) -> str:
+    """Render Markdown content to HTML."""
+    md = markdown.Markdown(
+        extensions=[
+            "markdown.extensions.tables",
+            "markdown.extensions.fenced_code",
+            "markdown.extensions.codehilite",
+            "pymdownx.betterem",
+            "pymdownx.emoji",
+            "pymdownx.highlight",
+            "pymdownx.magiclink",
+            "pymdownx.saneheaders",
+            "pymdownx.tasklist",
+            "pymdownx.tilde",
+            "nl2br",
+        ],
+        output_format="html",
+        extension_configs={
+            "pymdownx.emoji": {
+                "emoji_index": emoji.gemoji,
+                "emoji_generator": emoji.to_svg,
+            },
+            "pymdownx.tasklist": {
+                "custom_checkbox": True,
+            },
+        },
     )
+    return md.convert(content)
