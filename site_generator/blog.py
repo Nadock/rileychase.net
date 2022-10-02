d9da0d6d5e36271633c90fc504729e6c96737bd9
Riley Chase <riley.chase@rileychase.net>
2022-10-02 16:49:22 +1030

    Generate post previews from first paragraph

--- site_generator/blog.py
+++ site_generator/blog.py
@@ -1,36 +1,47 @@
 import pathlib
 
+import bs4
+
 from site_generator import config, logging, markdown
 
 LOGGER = logging.getLogger()
 
 
-async def blog_index_render_info(
-    cfg: config.SiteGeneratorConfig, path: pathlib.Path
-) -> dict:
+async def find_blog_posts(cfg: config.SiteGeneratorConfig, path: pathlib.Path) -> list:
     """
     Generate and return all the additional info the `"blog_index"` page type requires.
 
     The `path` should be the path to the `"blog_index"` markdown page.
     """
+    search_dir = path.parent
+
     posts = []
-    async for post in markdown.find_markdown(path.parent):
-        _, fm = await markdown.load_markdown(post)
-        fm.config = cfg
-        post_path = post.relative_to(path.parent)
-        if post_path == pathlib.Path("index.md"):
+    async for post in markdown.find_markdown(search_dir):
+        if post == path:
             continue
+
+        # Read post contents & frontmatter
+        content, fm = await markdown.load_markdown(cfg, post)
         if not fm.date:
-            raise ValueError(f"Cannot render blog post without a date: {post_path}")
+            raise ValueError("Cannot render blog post without a date")
+
+        # Generate a preview of the post
+        content = await markdown.render(content)
+        first_p = bs4.BeautifulSoup(content, features="html.parser").find("p")
+        preview = (
+            first_p.get_text()
+            if first_p
+            else "This page has no paragraphs, please add some content!"
+        )
 
         posts.append(
             {
                 "path": f"/{fm.get_output_path().relative_to(cfg.output)}",
-                "fm": fm,
+                "preview": preview,
+                **fm.get_props(),
             }
         )
 
-    posts.sort(key=lambda p: p["fm"].date)
-    return {
-        "posts": posts,
-    }
+    # Sort post by their date
+    posts.sort(key=lambda p: p["date"])
+    return posts
