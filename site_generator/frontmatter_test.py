f4f3a926b87789fcc1597b8f39567776568c4e18
Riley Chase <riley.chase@rileychase.net>
2025-05-13 20:10:01 +0930

    Remove unnessisary tests

--- site_generator/frontmatter_test.py
+++ site_generator/frontmatter_test.py
@@ -1,4 +1,3 @@
-import datetime
 import pathlib
 
 import pytest
@@ -10,35 +9,6 @@ def fake_page_frontmatter(**kwargs) -> frontmatter.PageFrontmatter:
     if "file" not in kwargs:
         kwargs["file"] = pathlib.Path("./pages/test.md").absolute()
     return frontmatter.PageFrontmatter(**kwargs)
-
-
-@pytest.mark.parametrize(
-    ("fm_kwargs", "cfg_kwargs", "expected"),
-    [
-        ({}, {}, ["", "default.html", "default.html"]),
-        ({"template": "foo.html"}, {}, ["foo.html", "default.html", "default.html"]),
-        ({}, {"default_template": "bar.html"}, ["", "default.html", "bar.html"]),
-        (
-            {"template": "foo.html"},
-            {"default_template": "bar.html"},
-            ["foo.html", "default.html", "bar.html"],
-        ),
-        ({"type": "blog"}, {}, ["", "blog.html", "default.html"]),
-    ],
-)
-def test_page_frontmatter__get_template_names(
-    fm_kwargs: dict, cfg_kwargs: dict, expected: list[str]
-) -> None:
-    fm = fake_page_frontmatter(**fm_kwargs)
-    fm.config = config_test.fake_test_config(**cfg_kwargs)
-
-    assert fm.get_template_names() == expected
-
-
-def test_page_frontmatter__get_template_names__no_config() -> None:
-    fm = fake_page_frontmatter()
-    with pytest.raises(ValueError, match="config must be set"):
-        fm.get_template_names()
 
 
 @pytest.mark.parametrize(
@@ -59,71 +29,3 @@ def test_page_frontmatter__get_output_path__no_config() -> None:
     fm = fake_page_frontmatter()
     with pytest.raises(ValueError, match="config must be set"):
         fm.get_output_path()
-
-
-@pytest.mark.parametrize(
-    ("fm_kwargs", "expected"),
-    [
-        (
-            {"file": pathlib.Path("./pages/debug/markdown.md").resolve()},
-            {
-                "tags": [],
-                "type": "default",
-                "debug": False,
-                "og": frontmatter.OpenGraphFrontmatter(
-                    url="https://localhost/debug/markdown", type="website"
-                ),
-            },
-        ),
-        (
-            {
-                "title": "test title",
-                "subtitle": None,
-                "file": pathlib.Path("./pages/debug/markdown.md").resolve(),
-            },
-            {
-                "title": "test title",
-                "tags": [],
-                "type": "default",
-                "debug": False,
-                "og": frontmatter.OpenGraphFrontmatter(
-                    title="test title",
-                    url="https://localhost/debug/markdown",
-                    type="website",
-                ),
-            },
-        ),
-        (
-            {
-                "template": "props_template",
-                "title": "props_title",
-                "subtitle": "props_subtitle",
-                "tags": ["tag_a", "tag_b"],
-                "date": datetime.date(2022, 1, 1),
-                "meta": {"foo": "bar"},
-                "file": pathlib.Path("./pages/debug/markdown.md").resolve(),
-                "config": config_test.fake_test_config(),
-                "type": "default",
-            },
-            {
-                "template": "props_template",
-                "title": "props_title",
-                "subtitle": "props_subtitle",
-                "tags": ["tag_a", "tag_b"],
-                "date": datetime.date(2022, 1, 1),
-                "type": "default",
-                "debug": False,
-                "og": frontmatter.OpenGraphFrontmatter(
-                    title="props_title",
-                    description="props_subtitle",
-                    url="https://localhost/debug/markdown",
-                    type="website",
-                ),
-            },
-        ),
-    ],
-)
-def test_page_frontmatter__get_props(fm_kwargs: dict, expected: dict) -> None:
-    fm = fake_page_frontmatter(**fm_kwargs)
-    fm.config = config_test.fake_test_config()
-    assert fm.get_props() == expected
