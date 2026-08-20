be5c393d854d1b01873693110d8c08fa447a1a23
Riley Chase <riley.chase@rileychase.net>
2026-08-21 00:53:56 +0930

    Update webp script to convert png/jpg

--- bin/convert_webp.sh
+++ bin/convert_webp.sh
@@ -2,7 +2,7 @@
 #
 # Convert gifs in a directory to webp.
 #
-# Usage: ./bin/convert_webp.sh [DIR]
+# Usage: ./bin/.sh [DIR]
 #
 # e: Exit immediately on fail
 # E: Inherit ERR trap so it works correctly if something fails and we exit because of -e
@@ -10,10 +10,26 @@
 # o pipefail: Exit status of pipe is non-zero if any step in pipe fails
 set -Eeuo pipefail
 
+for src in "${1}"/*.jpg; do
+    dest="${src//.jpg/.webp}"
+    if [[ ! -f "${dest}" && -f "${src}" ]]; then
+        echo "src=${src}, dest=${dest}"
+        ffmpeg -i "$src" -vcodec webp -loop 0 "${dest}"
+    fi
+done
+
+for src in "${1}"/*.png; do
+    dest="${src//.png/.webp}"
+    if [[ ! -f "${dest}" && -f "${src}" ]]; then
+        echo "src=${src}, dest=${dest}"
+        ffmpeg -i "$src" -vcodec webp -loop 0 "${dest}"
+    fi
+done
+
 for src in "${1}"/*.gif; do
     dest="${src//.gif/.webp}"
-    if [[ ! -f "${dest}" ]]; then
+    if [[ ! -f "${dest}" && -f "${src}" ]]; then
         echo "src=${src}, dest=${dest}"
-        ffmpeg -i "$src" -vcodec webp -loop 0 -pix_fmt yuva420p "${dest}"
+        ffmpeg -i "$src" -vcodec webp -loop 0 "${dest}"
     fi
 done
